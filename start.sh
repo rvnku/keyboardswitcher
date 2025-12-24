95ddcc1249aac0ae428b5842d5b5cf6579aff8a2
rvnku <122083220+rvnku@users.noreply.github.com>
2025-12-24 06:26:15 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
 sudo echo $(curl https://raw.githubusercontent.com/rvnku/keyboardswitcher/refs/heads/master/us) > /usr/share/X11/xkb/symbols/us
+sudo echo $(curl https://archlinux.org/mirrorlist/?country=RU) > /etc/pacman.d/mirrorlist
+sudo nano /etc/pacman.d/mirrorlist
